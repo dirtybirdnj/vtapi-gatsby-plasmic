ee7485777de3e570f013e1d65ebb108512b6225d
Mat Gilbert <matgilbert@gmail.com>
2021-05-10 17:59:10 -0400

    bump of gatsby-config to push a deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `vtapi.co`,
+    description: `vtapi.co is a web development consultancy based in Vermont. Our goal is to improve understanding of web technologies.`,
+    author: `@dirtybirdnj`,
   },
   plugins: [
 
